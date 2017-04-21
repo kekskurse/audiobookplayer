2004269a3596241611535865857db80efd3070cd
soeren <git@kekskurse.de>
2017-04-21 13:25:27 +0200

    Audibook time

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ var config = {
 var currentPlaylist = [];
 var currentTrack = 0;
 var currentName = "";
+var currentBookID = -1;
 
 var audio = document.getElementById("audiobookPlayerElement");
 //database
@@ -71,6 +72,7 @@ function loadConfig()
     config = data;
     console.log(config);
     init();
+    allAlbumLength();
   });
   }
 });
@@ -110,12 +112,67 @@ function loadAudio(name)
             {
                 currentTrack = config.audiobooks[i].currentTrack;
                 startpos = config.audiobooks[i].currentPos;
+                config.audiobooks[i].trackCount = res.length;
             }
         }
         playTrack(startpos);
       }
     });
 }
+
+function calculateAlbumLenght(id)
+{
+    console.log("Album lenght "+id)
+
+    var mp3Duration = require('mp3-duration');
+    var _id = id;
+    var _audiobook = config.audiobooks[id].name;
+    var _trackcount = 0;
+    var _doneTracks = 0;
+    console.log("Album lenght >"+_audiobook+"<")
+    return new Promise(function (fulfill, reject){
+        if(_audiobook=="")
+        {
+            fulfill();
+        }
+        getDirectories(config.path+"/"+_audiobook, function (err, res) {
+            _trackcount = res.length;
+            config.audiobooks[_id].trackCount = _trackcount;
+            for(var g=0;g<res.length;g++)
+            {
+                mp3Duration(res[g], function (err, duration) {
+                    if (err) reject(err.message);
+                    config.audiobooks[_id].duration = config.audiobooks[_id].duration + duration;
+                    console.log('Your file is ' + duration + ' seconds long ('+_audiobook+')');
+                    _doneTracks++;
+                    if(_doneTracks == _trackcount)
+                    {
+                        fulfill();
+                    }
+                });
+            }
+        });
+    });
+}
+function allAlbumLength(i = 0)
+{
+    var next = i + 1;
+    if(config.audiobooks[i]!=undefined)
+    {
+        if(true)//config.audiobooks[i].duration == undefined)
+        {
+            var r = calculateAlbumLenght(i);
+            r.then(function() {
+                allAlbumLength(next)
+            });
+        }
+        else {
+            allAlbumLength(next)
+        }
+    }
+}
+
+
 function playTrack(startpos = 0)
 {
     $(".playing").each(function(index, obj){
@@ -180,8 +237,37 @@ function createLiberyPage(audiobooks)
     for(var i = 0; i < audiobooks.length; i++)
     {
         html = html + '<div class="col-xs-4 col-md-3 libclick" data-name="'+audiobooks[i]+'"><a href="#" class="thumbnail">';
-        html = html +'<img style="width:100%;max-height:200px;" src="http://img.docker.kekskurse.de/img.php?q=audiobook '+audiobooks[i]+'" alt="'+audiobooks[i]+'">';
+        html = html +'<img style="width:100%;height:200px;" src="http://img.docker.kekskurse.de/img.php?q=audiobook '+audiobooks[i]+'" alt="'+audiobooks[i]+'">';
+        var _track = 0;
+        var _pos = 0;
+        var _alltracks = "NaN"
+        var _duration = undefined;
+        for(var g = 0; g < config["audiobooks"].length; g++)
+        {
+            if(config.audiobooks[g].name==audiobooks[i])
+            {
+                _track = config.audiobooks[g].currentTrack;
+                _pos = config.audiobooks[g].currentPos;
+                _alltracks = config.audiobooks[g].trackCount;
+                _duration = config.audiobooks[g].duration;
+            }
+        }
+        if(_track==0 && _pos == 0)
+        {
+            html = html + '<div class="new">Neu</div>';
+        }
+        else {
+            if(_duration!=undefined)
+            {
+                html = html + '<div class="new">Track '+_track+'/'+_alltracks+' ('+Math.round(_duration/60)+' min.)</div>';
+            }
+            else {
+                html = html + '<div class="new">Track '+_track+'/'+_alltracks+'</div>';
+            }
+
+        }
         html = html + '</a></div>';
+
     }
     html = html + "</div>";
     $("#libraryView").html(html);
